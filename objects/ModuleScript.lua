cc7cd4c997b1f0f08e54b09abf1796a28e15214c
Mana-scripts <madsbrriinckbastholm@gmail.com>
2026-03-01 13:55:20 +0100

    Update ModuleScript.lua

--- objects/ModuleScript.lua
+++ objects/ModuleScript.lua
@@ -2,16 +2,6 @@ local ModuleScript = {}
 
 --local getConstants = debug.getconstants or getconstants or getconsts
 
-function ModuleScript.new(instance)
-    local moduleScript = {}
-    local closure = getScriptClosure(instance)
 
-    moduleScript.Instance = instance
-    moduleScript.Constants = debug.getconstants(closure)
-    moduleScript.Protos = debug.getprotos(closure)
-    --moduleScript.ReturnValue = require(instance) // causes detection
-
-    return moduleScript
-end
 
 return ModuleScript
