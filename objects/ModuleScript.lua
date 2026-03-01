41ce8a722b3788a7f4bf462656c5aba9965895b6
Mana-scripts <madsbrriinckbastholm@gmail.com>
2026-03-01 13:46:12 +0100

    Update ModuleScript.lua

--- objects/ModuleScript.lua
+++ objects/ModuleScript.lua
@@ -1,11 +1,23 @@
 local ModuleScript = {}
 
+local function _getConstants(func)
+    if typeof(func) ~= "function" then
+        error("Expected function", 2)
+    end
+    
+    if not debug.getconstants then
+        error("debug.getconstants is not available in this environment", 2)
+    end
+    
+    return debug.getconstants(func)
+end
+
 function ModuleScript.new(instance)
     local moduleScript = {}
     local closure = getScriptClosure(instance)
 
     moduleScript.Instance = instance
-    moduleScript.Constants = getConstants(closure)
+    moduleScript.Constants = _getConstants(closure)
     moduleScript.Protos = getProtos(closure)
     --moduleScript.ReturnValue = require(instance) // causes detection
 
